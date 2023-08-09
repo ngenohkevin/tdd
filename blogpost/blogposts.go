4bcf837fe70b441f387c7f86cc03b0d0bb7ad12f
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-09 11:44:06 +0300

    new functions

--- blogpost/blogposts.go
+++ blogpost/blogposts.go
@@ -2,6 +2,7 @@ package blogposts
 
 import (
 	"errors"
+	"io"
 	"io/fs"
 )
 
@@ -23,4 +24,23 @@ func NewPostsFromFS(filesystem fs.FS) ([]Post, error) {
 		posts = append(posts, Post{})
 	}
 	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := fileSystem.Open(f.Name())
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+	return newPost(postFile)
+}
+
+func newPost(postFile fs.File) (Post, error) {
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+
+	post := Post{Title: string(postData)[7:]}
+	return post, nil
 }
