ff2fae248013289450b7054272e6903eac2a4548
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-09 11:39:35 +0300

    new mapFS

--- blogpost/blogposts_test.go
+++ blogpost/blogposts_test.go
@@ -8,13 +8,15 @@ import (
 
 func TestNewBlogPosts(t *testing.T) {
 	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
+		"hello world.md":  {Data: []byte("Title: Post 1")},
+		"hello-world2.md": {Data: []byte("Title: Post 2")},
 	}
 	posts, err := blogposts.NewPostsFromFS(fs)
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	_, err = blogposts.NewPostsFromFS(blogposts.StubFailingFS{})
 
 	if len(posts) != len(fs) {
 		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
