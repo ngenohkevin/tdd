8f530b0fc71938a626a7da6a3af441cadef4c7e3
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-09 11:06:33 +0300

    new method

--- blogpost/blogposts.go
+++ blogpost/blogposts.go
@@ -1,10 +1,18 @@
 package blogposts
 
 import (
+	"errors"
 	"io/fs"
 )
 
 type Posts struct {
+}
+
+type StubFailingFS struct {
+}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("oh no, i always fail")
 }
 
 func NewPostsFromFS(filesystem fs.FS) ([]Posts, error) {
